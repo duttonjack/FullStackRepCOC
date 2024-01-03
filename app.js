f990ccbe036b20dd32e185e365aec148fe71ca34
Jack Dutton <duttonjack@gmail.com>
2024-01-03 13:08:59 -0700

    Implemented post routes in server for owner and business

--- app.js
+++ app.js
@@ -33,7 +33,26 @@ app.get("/api/coc/business", (req, res) => {
     .catch((error) => res.status(500).json("Error Retriving Entries"))
 })
 
+app.post("/api/coc/business", (req, res) => {
+    const [ entry ] = req.body
+    console.log("in server Post: ", entry.name, entry.numEmployees, entry.ownerId)
+    pool.query('INSERT INTO business (name, num_employees, owner_id) VALUES ($1, $2, $3)', [ entry.name, entry.numEmployees, entry.ownerId ])
+    .then((result) => {
+        console.log(result.rows)
+        res.status(200).json("Sucessfully Added Entry to business")
+    })
+    .catch((error) => res.status(500).json("Server Error adding to business DB"))
+})
 
+app.post("/api/coc/owner", (req, res) => {
+    const [ entry ] = req.body
+    console.log("in server Post: ", entry.name, entry.age,)
+    pool.query('INSERT INTO owner (name, age) VALUES ($1, $2)', [ entry.name, entry.age ])
+    .then((result) => {
+        res.status(200).json("Sucessfully Added Entry to owner")
+    })
+    .catch((error) => res.status(500).json("Server Error adding to owner DB"))
+})
 
 
 app.listen(expressPort, () => console.log(`Listening on port ${expressPort}`))
