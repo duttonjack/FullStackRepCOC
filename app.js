9f8fc0ca882a7e335059c357dd2dc09c1d52e7f4
Jack Dutton <duttonjack@gmail.com>
2024-01-03 14:22:20 -0700

    added patch route in server for both tables, verified working onlocal db

--- app.js
+++ app.js
@@ -17,19 +17,13 @@ app.use(express.static('public'))
 
 app.get("/api/coc/owner", (req, res) => {
     pool.query('SELECT * FROM owner')
-    .then((result) => {
-        console.log(result.rows)
-        res.status(200).json("Sucessfully Retrived Entries")
-    })
+    .then((result) => res.status(200).json("Sucessfully Retrived Entries"))
     .catch((error) => res.status(500).json("Error Retriving Entries"))
 })
 
 app.get("/api/coc/business", (req, res) => {
     pool.query('SELECT * FROM business')
-    .then((result) => {
-        console.log(result.rows)
-        res.status(200).json("Sucessfully Retrived Entries")
-    })
+    .then((result) => res.status(200).json("Sucessfully Retrived Entries"))
     .catch((error) => res.status(500).json("Error Retriving Entries"))
 })
 
@@ -37,20 +31,14 @@ app.post("/api/coc/business", (req, res) => {
     const [ entry ] = req.body
     console.log("in server Post: ", entry.name, entry.numEmployees, entry.ownerId)
     pool.query('INSERT INTO business (name, num_employees, owner_id) VALUES ($1, $2, $3)', [ entry.name, entry.numEmployees, entry.ownerId ])
-    .then((result) => {
-        console.log(result.rows)
-        res.status(200).json("Sucessfully Added Entry to business")
-    })
+    .then((result) => res.status(200).json("Sucessfully Added Entry to business"))
     .catch((error) => res.status(500).json("Server Error adding to business DB"))
 })
 
 app.post("/api/coc/owner", (req, res) => {
     const [ entry ] = req.body
-    console.log("in server Post: ", entry.name, entry.age,)
     pool.query('INSERT INTO owner (name, age) VALUES ($1, $2)', [ entry.name, entry.age ])
-    .then((result) => {
-        res.status(200).json("Sucessfully Added Entry to owner")
-    })
+    .then((result) => res.status(200).json("Sucessfully Added Entry to owner"))
     .catch((error) => res.status(500).json("Server Error adding to owner DB"))
 })
 
@@ -66,6 +54,22 @@ app.delete("/api/coc/owner/:id", (req, res) => {
     pool.query('DELETE FROM owner WHERE id = $1', [ id ])
     .then((result) => res.status(200).json('Entry deleted from owner'))
     .catch((error) => res.status(500).send('Error deleting entry from owner'))
+})
+
+app.patch("/api/coc/owner/:id", (req, res) => {
+    const id = req.params.id
+    const [ body ] = req.body
+    pool.query('UPDATE owner SET name = $1 WHERE id =  $2', [ body.name, id ])
+    .then((result) => res.status(200).json("Entry in owner updated"))
+    .catch((error) => res.status(500).json("Server Error updating entry in owner"))
+})
+
+app.patch("/api/coc/business/:id", (req, res) => {
+    const id = req.params.id
+    const [ body ] = req.body
+    pool.query('UPDATE business SET name = $1 WHERE id =  $2', [ body.name, id ])
+    .then((result) => res.status(200).json("Entry in business updated"))
+    .catch((error) => res.status(500).json("Server Error updating entry in business"))
 })
 
 app.listen(expressPort, () => console.log(`Listening on port ${expressPort}`))
