52dd077683866a7509969b5f44fd9da813769850
Jack Dutton <duttonjack@gmail.com>
2024-01-03 14:06:03 -0700

    Implemented delete routes on server and verfied working with local database

--- app.js
+++ app.js
@@ -54,5 +54,18 @@ app.post("/api/coc/owner", (req, res) => {
     .catch((error) => res.status(500).json("Server Error adding to owner DB"))
 })
 
+app.delete("/api/coc/business/:id", (req, res) => {
+    const id = req.params.id
+    pool.query('DELETE FROM business WHERE id = $1', [ id ])
+    .then((result) => res.status(200).json('Entry deleted from business'))
+    .catch((error) => res.status(500).send('Error deleting entry from business'))
+})
+
+app.delete("/api/coc/owner/:id", (req, res) => {
+    const id = req.params.id
+    pool.query('DELETE FROM owner WHERE id = $1', [ id ])
+    .then((result) => res.status(200).json('Entry deleted from owner'))
+    .catch((error) => res.status(500).send('Error deleting entry from owner'))
+})
 
 app.listen(expressPort, () => console.log(`Listening on port ${expressPort}`))
